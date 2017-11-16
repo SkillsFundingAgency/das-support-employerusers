7397bf867c2f126d55897791d6f7867a325c591b
DESKTOP-9L164P1\Engineer <samuel.dev.dba@gmail.com>
2017-11-16 15:01:40 +0000

    Add Logging

--- src/SFA.DAS.EmployerUsers.Support.Web/Global.asax.cs
+++ src/SFA.DAS.EmployerUsers.Support.Web/Global.asax.cs
@@ -9,37 +9,24 @@ namespace SFA.DAS.EmployerUsers.Support.Web
 {
     public class MvcApplication : System.Web.HttpApplication
     {
-        private ILog _logger;
-
-        public MvcApplication()
-        {
-            _logger = DependencyResolver.Current.GetService<ILog>();
-        }
-
         protected void Application_Start()
         {
             MvcHandler.DisableMvcResponseHeader = true;
             var logger = DependencyResolver.Current.GetService<ILog>();
-
-            //logger.Info("Starting Web Role");
+            logger.Info("Starting Web Role");
 
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
 
-            //logger.Info("Web Role started");
+            logger.Info("Web role started");
         }
 
         protected void Application_Error(object sender, EventArgs e)
         {
             Exception ex = Server.GetLastError().GetBaseException();
-            //var logger = DependencyResolver.Current.GetService<ILog>();
-
-            //if (ex is HttpException
-            //    && ((HttpException)ex).GetHttpCode() != 404)
-            //{
-                //logger.Error(ex, "App_Error");
-            //}
+            var logger = DependencyResolver.Current.GetService<ILog>();
+            logger.Error(ex, "App_Error");
         }
     }
 }
