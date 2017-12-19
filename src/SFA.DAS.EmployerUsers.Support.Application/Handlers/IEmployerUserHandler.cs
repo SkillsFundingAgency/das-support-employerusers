66108673c31fc57aa0c8b55e0e21abccf2108853
Iain.Stevenson <Iain.Stevenson@live.co.uk>
2017-12-19 15:55:42 +0000

    Now uses the Support shared package

--- src/SFA.DAS.EmployerUsers.Support.Application/Handlers/IEmployerUserHandler.cs
+++ src/SFA.DAS.EmployerUsers.Support.Application/Handlers/IEmployerUserHandler.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
-using ESFA.DAS.Support.Shared;
+using SFA.DAS.Support.Shared;
 
 namespace SFA.DAS.EmployerUsers.Support.Application.Handlers
 {
