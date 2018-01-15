02abaeb0b485ca289739804b53fb4fedf8dbac80
kola-samuel <samuel.dev.dba@gmail.com>
2018-01-15 15:28:06 +0000

    Ascs 35 filtering on the support console after merge (#12)
    * Error handling changes
    
    * Error page changes
    
    * Add logger to user service

--- src/SFA.DAS.EmployerUsers.Support.Application/Handlers/EmployerUserHandler.cs
+++ src/SFA.DAS.EmployerUsers.Support.Application/Handlers/EmployerUserHandler.cs
@@ -5,23 +5,36 @@ using SFA.DAS.EmployerUsers.Support.Infrastructure;
 using SFA.DAS.EmployerUsers.Support.Core.Domain.Model;
 using System;
 using SFA.DAS.Support.Shared.SearchIndexModel;
+using SFA.DAS.NLog.Logger;
 
 namespace SFA.DAS.EmployerUsers.Support.Application.Handlers
 {
     public class EmployerUserHandler : IEmployerUserHandler
     {
         private readonly IEmployerUserRepository _userRepository;
+        private readonly ILog _log;
 
-        public EmployerUserHandler(IEmployerUserRepository userRepository)
+        public EmployerUserHandler(IEmployerUserRepository userRepository, ILog log)
         {
             _userRepository = userRepository;
+            _log = log;
         }
 
         public async Task<IEnumerable<UserSearchModel>> FindSearchItems()
         {
+            try
+            {
 
-            var models = await _userRepository.FindAllDetails();
-            return models.Select(m => Map(m)).ToList();
+                var models = await _userRepository.FindAllDetails();
+                return models.Select(m => Map(m)).ToList();
+            }
+            catch (Exception ex)
+            {
+                _log.Error(ex, "Error while trying to load users");
+
+                throw;
+            }
+
         }
 
         public UserSearchModel Map(EmployerUser employerUser)
@@ -29,13 +42,13 @@ namespace SFA.DAS.EmployerUsers.Support.Application.Handlers
             return new UserSearchModel
             {
                 Id = employerUser.Id,
-                Email =  string.IsNullOrEmpty(employerUser.Email) ? "NA" : employerUser.Email,
+                Email = string.IsNullOrEmpty(employerUser.Email) ? "NA" : employerUser.Email,
                 FirstName = employerUser.FirstName,
                 LastName = employerUser.LastName,
                 Status = Enum.GetName(typeof(UserStatus), employerUser.Status),
                 SearchType = SearchCategory.User
             };
         }
-      
+
     }
 }
