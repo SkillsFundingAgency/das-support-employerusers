d1494c3f106f84a4c1e4ac28003a319cedb3e62b
Iain.Stevenson <Iain.Stevenson@live.co.uk>
2018-02-06 16:19:24 +0000

    Added Search controller

---
+++ src/SFA.DAS.EmployerUsers.Support.Web/Controllers/SearchController.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using System.Web.Http;
+using SFA.DAS.EmployerUsers.Support.Application.Handlers;
+
+namespace SFA.DAS.EmployerUsers.Support.Web.Controllers
+{
+    [RoutePrefix("api/search")]
+    public class SearchController : ApiController
+    {
+        private readonly IEmployerUserHandler _handler;
+
+        public SearchController(IEmployerUserHandler handler)
+        {
+            _handler = handler;
+        }
+
+
+        [HttpGet]
+        public async Task<IHttpActionResult> Users()
+        {
+            var model = await _handler.FindSearchItems();
+            return Json(model);
+        }
+
+    }
+}
